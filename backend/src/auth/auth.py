8d687c6cd77a4c32f681162cf5daeade48f4946f
Aurora <82787709+adap597@users.noreply.github.com>
2021-06-16 19:21:51 -0700

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -10,6 +10,7 @@ ALGORITHMS = ['RS256']
 API_AUDIENCE = 'Coffee'
 CLIENT_ID = '4aM4B8RYBlFQNrMcIwOBhc7SYQM3jfCG'
 
+
 # AuthError Exception
 '''
 AuthError Exception
@@ -65,18 +66,17 @@ def get_token_auth_header():
 
 
 def check_permissions(permission, payload):
-    # raise Exception('Not Implemented')
     if 'permissions' not in payload:
-        raise AuthError({
-            'code': 'invalid_claims',
-            'description': 'Permissions not included.'
-        }, 400)
+                        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
 
     if permission not in payload['permissions']:
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permission not found.'
-        }, 401)
+        }, 403)
     return True
 
 '''
@@ -89,7 +89,6 @@ def check_permissions(permission, payload):
 
 
 def verify_decode_jwt(token):
-    # raise Exception('Not Implemented')
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
@@ -109,37 +108,38 @@ def verify_decode_jwt(token):
                 'n': key['n'],
                 'e': key['e']
             }
-        if rsa_key:
-            try:
-                payload = jwt.decode(
-                    token,
-                    rsa_key,
-                    algorithms=ALGORITHMS,
-                    audience=API_AUDIENCE,
-                    issuer='https://' + AUTH0_DOMAIN + '/'
-                )
-                return payload
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
 
-            except jwt.ExpiredSignatureError:
-                raise AuthError({
-                    'code': 'token_expired',
-                    'description': 'Token expired'
-                }, 401)
-            except jwt.JWTClaimsError:
-                raise AuthError({
-                    'code': 'invalid_claims',
-                    'description':
-                    'Incorrect cliams. Please, check the audience and issuer.'
-                }, 401)
-            except Exception:
-                raise AuthError({
-                    'code': 'invalid_header',
-                    'description': 'Unable to parse authentication token',
-                }, 400)
-        raise AuthError({
-            'code': 'invalid_code',
-            'description': 'Unable to find the appropriate key.'
-        }, 400)
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
 
 
 def requires_auth(permission=''):
@@ -147,7 +147,7 @@ def requires_auth(permission=''):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            payload = verify_decode_jwt
+            payload = verify_decode_jwt(token)
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
 
